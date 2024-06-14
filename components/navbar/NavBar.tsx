2dca256088bb1a9d7257ef256ed42423b38f3d09
CESCO <cescovalledev@gmail.com>
2024-06-13 18:03:52 -0600

    navbar dynamic

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,17 +1,25 @@
+import { HomeIcon } from "@primer/octicons-react";
+import Link from "next/link";
+
+const navItems = [
+  { path: "/about", text: "About" },
+  { path: "/contact", text: "Contact" },
+  { path: "/pricing", text: "Pricing" },
+];
+
 export const NavBar = () => {
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
+      <Link href={"/"} className="flex items-center gap-1">
+        <HomeIcon />
+        <span>Home</span>
+      </Link>
       <div className="flex flex-1"></div>
-      <a className="mr-2" href="/about">
-        About
-      </a>
-      <a className="mr-2" href="/contact">
-        Contact
-      </a>
-      <a className="mr-2" href="/pricing">
-        Pricing
-      </a>
+      {navItems.map((navItem) => (
+        <Link key={navItem.path} className="mr-2" href={navItem.path}>
+          {navItem.text}
+        </Link>
+      ))}
     </nav>
   );
 };
